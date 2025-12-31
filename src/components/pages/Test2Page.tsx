9d2789eb0185ac0d313163536a98067a4a58aeb5
Wix Remote Machine <support@wix.com>
2025-12-31 10:55:09 +0000

    Manual code changes

--- src/components/pages/Test2Page.tsx
+++ src/components/pages/Test2Page.tsx
@@ -9,12 +9,7 @@ export default function TestPage() {
   return (
     <div className="min-h-screen bg-background">
       // Render
-   <CategoryList.Root categoriesListConfig={categoriesListConfig}>
-      <CategoryList.Loading>Loading...</CategoryList.Loading>
-      <CategoryList.CategoryRepeater>
-        <Category.Label />
-      </CategoryList.CategoryRepeater>
-    </CategoryList.Root>
+ 
     </div>
   );
 }
