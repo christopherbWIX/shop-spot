00bb2281d282f01f29751b6081ef9599f2012e2f
Wix Remote Machine <support@wix.com>
2025-12-31 10:51:52 +0000

    Manual code changes

--- src/components/pages/Test2Page.tsx
+++ src/components/pages/Test2Page.tsx
@@ -1,11 +1,14 @@
 
 import { loadCategoriesListServiceConfig } from '@wix/stores/services';
-import { CategoryList } from '@wix/stores/components/react';
+import { CategoryList } from '@wix/stores/components';
 
 // Load categories data during SSR
 const categoriesConfig = await loadCategoriesListServiceConfig();
 
-
+export default function TestPage() {
+  return (
+    <div className="min-h-screen bg-background">
+      // Render
 <CategoryList.Root categoriesConfig={categoriesConfig}>
   <CategoryList.ItemContent>
     {({ category }) => (
