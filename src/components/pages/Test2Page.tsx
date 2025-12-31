146565459c55ce1372b375a83c9536b9f4b3b35e
Wix Remote Machine <support@wix.com>
2025-12-31 10:54:30 +0000

    Manual code changes

--- src/components/pages/Test2Page.tsx
+++ src/components/pages/Test2Page.tsx
@@ -1,5 +1,5 @@
 
-import { CategoryList } from '@wix/stores/components';
+import { Category, CategoryList } from '@wix/stores/components';
 import { loadCategoriesListServiceConfig } from '@wix/stores/services';
 
 // Load categories data during SSR
@@ -9,9 +9,12 @@ export default function TestPage() {
   return (
     <div className="min-h-screen bg-background">
       // Render
-<CategoryList.Root categoriesListConfig={categoriesListConfig}>
-
-</CategoryList.Root>
+   <CategoryList.Root categoriesListConfig={categoriesListConfig}>
+      <CategoryList.Loading>Loading...</CategoryList.Loading>
+      <CategoryList.CategoryRepeater>
+        <Category.Label />
+      </CategoryList.CategoryRepeater>
+    </CategoryList.Root>
     </div>
   );
 }
