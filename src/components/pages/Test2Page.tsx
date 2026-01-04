037a53c8b3b0a8d3bf2c6666f0c823fe93daff61
Wix Remote Machine <support@wix.com>
2026-01-04 16:02:43 +0000

    Manual code changes

--- src/components/pages/Test2Page.tsx
+++ src/components/pages/Test2Page.tsx
@@ -10,7 +10,7 @@ import { Category, CategoryList } from "@wix/headless-stores/react";
 import { loadCategoriesListServiceConfig } from '@wix/stores/services';
 const categoriesListConfig = await loadCategoriesListServiceConfig();
 
-import { Option } from "@wix/stores/components";
+import { Choice, Option } from "@wix/stores/components";
 
 import { useEffect, useState } from 'react';
 
